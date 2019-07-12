04100d104026862535f68ed61f05801b02da28aa
rusuranu <primat777@gmail.com>
2019-07-12 13:31:22 +0300

    update2

--- fw.ino
+++ fw.ino
@@ -2,26 +2,62 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <Adafruit_NeoPixel.h>
+
+#define ledpin 6 // pin to led matrix.
+#define ledCount 64 // led count
+#define brightness 130 //max brightness for matrix 
 
 const char* ssid = "........";
 const char* password = "........";
 
 ESP8266WebServer server(80);
+Adafruit_NeoPixel matrix(ledcount, ledpin, NEO_GRB + NEO_KHZ800);
+
+//fillcolor
+void fillCollor(int R, int G, int B) {
+  for(int i=0; i<matrix.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, R,G,B);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+  }
+}
+int colorValue(percent){
+  if ((percent>100)or(percent<0)){return 0;};
+  return (int) round(2.54*percent);
+  
+}
+
 // ON-request handler
 void handleOn(){
+ if( server.args()=3){
+   int R=colorValue(server.arg('R').toInt());
+   int G=colorValue(server.arg('G').toInt());
+   int B=colorValue(server.arg('B').toInt());
+   fillCollor(R,G,B);
+ }
+ else {fillCollor(254,254,254);} 
+  
 }
+
 //OFF-request handler
 void handleOff(){
+  matrix.clear();
 }
 
 //Setup
 void setup(void){
+  //matrix setup
+  matrix.begin();
+  matrix.show();
+  matrix.setBrightness(brightness);
+  //wifi setrup
   WiFi.begin(ssid, password);
- // Wait for connection
+  // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
- //set dns name 
+
+  //set dns name 
   if (MDNS.begin("timer")) {;
   }
   
